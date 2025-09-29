5282fd0a3f270175362abf246fb684561bae46a5
Uriel Cohen <uriel.c@sealsecurity.io>
2025-09-29 11:25:35 +0300

    cve

--- src/core/evaluator.js
+++ src/core/evaluator.js
@@ -4238,7 +4238,16 @@ class PartialEvaluator {
         isInternalFont = !!fontFile;
       }
     }
-
+    
+    let fontMatrix = dict.getArray("FontMatrix");
+     if (
+       !Array.isArray(fontMatrix) ||
+       fontMatrix.length !== 6 ||
+       fontMatrix.some(x => typeof x !== "number")
+     ) {
+       fontMatrix = FONT_IDENTITY_MATRIX;
+     }
+     
     properties = {
       type,
       name: fontName.name,
@@ -4252,7 +4261,7 @@ class PartialEvaluator {
       loadedName: baseDict.loadedName,
       composite,
       fixedPitch: false,
-      fontMatrix: dict.getArray("FontMatrix") || FONT_IDENTITY_MATRIX,
+      fontMatrix,
       firstChar,
       lastChar,
       toUnicode,
